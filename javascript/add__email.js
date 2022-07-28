9534ed0de15ba097104b7685bc3ce7a3a6ceda50
Dary-Boyce <contact@darylboyce.com>
2022-07-28 08:36:24 +0100

    Overhall of working javascript and layout.

--- javascript/add__email.js
+++ javascript/add__email.js
@@ -10,6 +10,7 @@ let obj;
 let i = 0;
 let new__user = true;
 let profile__name = [];
+let expand__details = document.getElementById ("user__expand");
 //
 //
 //Listens for click of the button labeled 'new__image' and assigns a new image to the main__pic
@@ -35,24 +36,60 @@ function store__user__data(){
 // 
 // 
 // 
-function find__user__data(){
-    let obj = user__data.find(user__data => user__data['name'] === input.value);
+function display__user__data(){
+   
+    // let wrap__button = document.createElement ("div")
 
-    console.log(user__data);
-    document.getElementById('main__display').innerHTML =  "<div id=${obj['name']} ><h3>${obj['name']}</h3><img src=${obj['url']} ></div>";
+    let display = document.createElement ("div");
+
+    let display_img = document.createElement ("img");
+
+    let element = document.getElementById ("main__display");
+
+    let newContent = document.createElement ("p");
+
+    let newContent__text = document.createTextNode(input.value);
+
+    let top = document.createElement("div");
+
+    let bottom = document.createElement("div");
+    element.appendChild(display);
+    display.setAttribute('class', "display__box__inner");
+    display.setAttribute('id', input.value )
+
+    display.appendChild(top);
+    display.appendChild(bottom);
+    top.setAttribute( 'class', "display__box__inner__top")
+    bottom.setAttribute( 'class', "display__box__inner__bottom")
+
+    top.appendChild(display_img);
+    display_img.setAttribute("src", pic__box.src);
+
+
+    bottom.appendChild(newContent);
+    newContent.appendChild(newContent__text);
+    
+
 }
 // 
 // 
 // 
-function additional__image(){
-    user__data[i].push(pic__box.src);
+function add__user__same() {
+    let exsisting__email = document.getElementById (input.value);
+
+    let display_img = document.createElement ("img");
+
+    let top_e = exsisting__email.firstChild;
+
+    top_e.appendChild(display_img);
+    display_img.setAttribute("src", pic__box.src);
+
 }
-// 
-// 
-// 
-// function add__display__box(){
-//     document.getElementById('main__display')
-// }
+//
+function same__image(){
+    
+} 
+//
 // 
 // Watches for click on button with value of 'submit' , runs checks for empty input value, checks for
 // matching email and then exicutes addemail function above if sucessful
@@ -60,22 +97,23 @@ submit.addEventListener( 'click',() => {
     if(input.value.length>0 && input.value.replace(/\s/g, '').length==0) { // string has only spaces } {
         console.log("No Input");
     }
+   
     else{
         for ( let i = 0; i < profile__name.length; i++){
-            if (profile__name.indexOf(input.value) === i){
+         if (profile__name.indexOf(input.value) === i){
                 new__user = false;
-                // user__data[i].push(pic__box.src);
+                add__user__same();
                 console.log(" Value exists!");
             }
         }
         if(new__user == true) {
             store__user__data();
-            find__user__data();
+            display__user__data();
             input.value = '';
             obj = "";
         }
         new__user = true;
-    }    
+        }       
     } 
 )
 
@@ -91,5 +129,3 @@ submit.addEventListener( 'click',() => {
 
 
 
-
-// user__data.find(user__data => user__data['name'] === input.value)
---
+++ javascript/show__hide__email.js
@@ -0,0 +1,31 @@
+
+
+
+
+// function myFunction() {
+//     var x = document.getElementById("myDIV");
+//     if (x.style.display === "none") {
+//       x.style.display = "block";
+//     } else {
+//       x.style.display = "none";
+//     }
+//   }
+
+
+// function expand__open(){
+//     let expand__details = document.getElementById ("user__expand");
+
+    
+// // }
+// function show__hide(){
+
+//      let w__button = document.getElementById ("user__expand")
+
+
+
+//     $('w__button').click(function(){
+//       alert("The paragraph was clicked.");
+//     });
+// };
+
+// show__hide();
